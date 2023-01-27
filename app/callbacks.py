dce81a993cbe958c27d2e3a96084277c5409a46c
=Insiro <leeon@insiro.me>
2023-01-27 17:26:11 +0900

    fix: callback make save when acc or loss changed

--- app/callbacks.py
+++ app/callbacks.py
@@ -13,18 +13,30 @@ class SaveModelCallbacak(Callback):
     def __init__(self, path_loader: PathLoader):
         super().__init__()
         self.save_dir = path_loader.get_save_path()
-        self.loss = None
+        self.train_loss = None
+        self.train_acc = None
+        self.test_acc = None
+        self.test_loss = None
 
     def on_epoch_end(self, epoch, logs: Optional[TrainingLog] = None):
         if logs is None:
             return
-        if self.loss is None or self.loss > logs.test_loss:
-            self.loss = logs.test_loss
-            filename = f"epoch{epoch}_loss{self.loss:.5f}_acc{logs.test_acc:.3f}"
+        if (
+            (self.train_loss is None)
+            or ((self.test_loss > logs.test_loss) or (self.test_acc < logs.test_acc))
+            or (
+                (self.train_loss > logs.train_loss) or (self.train_acc < logs.train_acc)
+            )
+        ):
+            self.test_loss = logs.test_loss
+            self.test_acc = logs.test_acc
+            self.train_loss = logs.train_loss
+            self.train_acc = logs.train_acc
+            filename = f"epoch{epoch}_loss{self.test_loss:.5f}_acc{logs.test_acc:.3f}"
             logs.model.save_weights(
                 path.join(self.save_dir, filename), save_format="h5"
             )
-            logging.info(f"loss is decreased, file saved to {filename}")
+            logging.warn(f"loss is decreased, file saved to {filename}")
 
 
 class SaveSummaryCallback(Callback):
@@ -40,14 +52,14 @@ class SaveSummaryCallback(Callback):
     def on_train_end(self, logs: Optional[TrainingLog] = None):
         if logs is None:
             return
-        logging.info(
+        logging.warn(
             f"\tAcc : {logs.train_acc}\tLoss: {logs.train_loss}\t lr : {float(logs.lr)}"
         )
 
     def on_test_end(self, logs: Optional[TrainingLog] = None):
         if logs is None:
             return
-        logging.info(f"\tAcc : {logs.test_acc}\tLoss: {logs.test_loss}")
+        logging.warn(f"\tAcc : {logs.test_acc}\tLoss: {logs.test_loss}")
 
     @tf.function
     def on_epoch_end(self, epoch, logs: Optional[TrainingLog] = None):

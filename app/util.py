eb2cca5cda9710c3ac4dc8a2a0027eda99b59ebf
=Insiro <leeon@insiro.me>
2023-01-27 17:28:48 +0900

    add: data loader for divided folder - disable shuffle option

--- app/util.py
+++ app/util.py
@@ -10,12 +10,13 @@ def convert(model):
     return coverter.convert()
 
 
-def load_data(config, dir="data"):
+def load_data(config, dir="data", split_val=False):
     data_path = config["dir"][dir]
     train_ds = tf.keras.preprocessing.image_dataset_from_directory(
         data_path,
         validation_split=config["valid_split"],
         subset="training",
+        shuffle=False,
         label_mode="categorical",
         seed=123,
         image_size=config["shape"],
@@ -25,17 +26,42 @@ def load_data(config, dir="data"):
         data_path,
         validation_split=config["valid_split"],
         subset="validation",
+        shuffle=False,
         label_mode="categorical",
         seed=123,
         image_size=config["shape"],
         batch_size=config["batch_size"],
     )
-    val_batches = tf.data.experimental.cardinality(val_ds)
+    test_ds = None
+    if split_val:
+        val_batches = tf.data.experimental.cardinality(val_ds)
 
-    test_ds = val_ds.take((2 * val_batches) // 2)
-    val_ds = val_ds.skip((2 * val_batches) // 2)
+        test_ds = val_ds.take((2 * val_batches) // 2)
+        val_ds = val_ds.skip((2 * val_batches) // 2)
     class_names = train_ds.class_names
-    return train_ds, test_ds, val_ds, class_names
+    return train_ds, val_ds, test_ds, class_names
+
+
+def load_data2(config):
+    dir = config["dir"]
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        dir["data"],
+        shuffle=False,
+        label_mode="categorical",
+        seed=123,
+        image_size=config["shape"],
+        batch_size=config["batch_size"],
+    )
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        dir["test"],
+        shuffle=False,
+        label_mode="categorical",
+        seed=123,
+        image_size=config["shape"],
+        batch_size=config["batch_size"],
+    )
+    class_names = train_ds.class_names
+    return train_ds, val_ds, None, class_names
 
 
 class TrainingLog:

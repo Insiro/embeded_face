6f78c5a2e80f16f17a607237a772f00568ca2dbb
=Insiro <leeon@insiro.me>
2023-02-12 15:43:13 +0900

    model: edit model remove batch normalization add outbout embbeding layer

--- models/model.py
+++ models/model.py
@@ -1,16 +1,17 @@
 import tensorflow as tf
 
+
 keras = tf.keras
 from keras.applications import MobileNet
 from keras.layers import (
-    BatchNormalization,
     Dense,
     GlobalAveragePooling2D,
     Input,
-    Softmax,
+    Flatten,
+    Lambda,
 )
 from keras.models import Model
-from .layers import ArcLayer
+from .layers import ArcLayer, ArcMarginPenaltyLogists
 
 
 class FaceMobile(Model):
@@ -19,11 +20,14 @@ class FaceMobile(Model):
         self.mobilenet = MobileNet(
             weights="imagenet", include_top=False, input_shape=(shape[0], shape[1], 3)
         )
-        self.avgpool = GlobalAveragePooling2D()
         self.mobilenet.trainable = False
-        self.batch_norm = BatchNormalization()
+        self.avgpool = GlobalAveragePooling2D()
+        self.flatten = Flatten()
         self.dense1 = Dense(1024, activation="relu")
-        self.dense2 = Dense(512, activation="relu")
+        self.dense2 = Dense(num_classes, activation="relu")
+        self.norm = Lambda(
+            lambda x: tf.math.l2_normalize(x, axis=-1), name="embeddings"
+        )
         self.arc = ArcLayer(num_classes)
 
     @tf.function
@@ -31,9 +35,10 @@ class FaceMobile(Model):
         x = inputs
         x = self.mobilenet(x)
         x = self.avgpool(x)
-        x = self.batch_norm(x)
+        x = self.flatten(x)
         x = self.dense1(x)
         x = self.dense2(x)
+        x = self.nrom(x)
         x = self.arc(x)
         return x
 
@@ -46,9 +51,9 @@ def build_face_model(num_calsses, shape=(480, 640)):
     mobilenet.trainable = False
     x = mobilenet(inputs)
     x = GlobalAveragePooling2D()(x)
-    x = BatchNormalization()(x)
+    x = Flatten()(x)
     x = Dense(1024, activation="relu")(x)
-    x = Dense(512, activation="relu")(x)
+    x = Dense(num_calsses, activation="relu")(x)
+    x = Lambda(lambda x: tf.math.l2_normalize(x, axis=-1), name="embeddings")(x)
     out = ArcLayer(num_calsses)(x)
-    x = Softmax()(x)
     return Model(inputs=inputs, outputs=out)

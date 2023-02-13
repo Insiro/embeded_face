f3e96681d4cb2574048b62969d0c245baf0cd876
=Insiro <leeon@insiro.me>
2023-02-13 18:17:15 +0900

    refactor: modularize trainer

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -8,52 +8,14 @@ keras = tf.keras
 from keras.callbacks import Callback
 from utils.util import TrainingLog
 
-
-class AccMatrix:
-    def __init__(self, train_loss, train_acc, test_loss, test_acc) -> None:
-        self.train_loss = train_loss
-        self.train_acc = train_acc
-        self.test_loss = test_loss
-        self.test_acc = test_acc
-
-    @tf.function
-    def reset_states(self):
-        self.train_acc.reset_states()
-        self.test_acc.reset_states()
-
-    def get_train_str(self):
-        return f"Accuracy: {self.train_acc.result() * 100}"
-
-    def get_test_str(self):
-        return f"Test Accuracy: {self.test_acc.result() * 100}"
-
-    def __str__(self) -> str:
-        return f"{self.get_train_str()}\t{self.get_train_str()}"
+from .base import AccMatrix, TrainerBase
 
 
-class ModelTrainer:
+class ModelTrainer(TrainerBase):
     def __init__(
-        self,
-        model,
-        logger,
-        loss_function,
-        optimizer,
-        acc_matrix: AccMatrix,
+        self, model, logger, loss_function, optimizer, acc_matrix: AccMatrix
     ) -> None:
-        self.model: tf.keras.Model = model
-        self.logger = logger
-        self.loss_function = loss_function
-        self.optimizer = optimizer
-        self.acc_matrix = acc_matrix
-        self.train_loss = tf.keras.metrics.Mean(name="train_loss")
-        self.test_loss = tf.keras.metrics.Mean(name="train_loss")
-        self.callback: list[Callback] = []
-
-    def add_callback(self, callback: Union[Callback, list[Callback]]):
-        if callback is Callback:
-            self.callback.append(callback)
-        else:
-            self.callback.extend(callback)
+        super().__init__(model, logger, loss_function, optimizer, acc_matrix)
 
     @tf.function
     def test_step(self, images, labels):
@@ -71,38 +33,3 @@ class ModelTrainer:
         self.optimizer.apply_gradients(zip(gradinents, self.model.trainable_variables))
         self.train_loss(loss)
         self.acc_matrix.train_acc(labels, pred)
-
-    def reset_states(self):
-        self.acc_matrix.reset_states()
-        self.train_loss.reset_states()
-        self.test_loss.reset_states()
-
-    def train(self, epochs, train_set, test_set):
-        for callback in self.callback:
-            callback.on_train_begin(self)
-        logs = TrainingLog(self.model)
-
-        for epoch in range(epochs):
-            self.reset_states()
-            for image, labels in tqdm(train_set, desc=f"train {epoch} epoch"):
-                self.train_step(image, labels)
-            logs.update_train(
-                self.acc_matrix.train_acc,
-                self.train_loss,
-                self.optimizer.lr,
-            )
-            for callback in self.callback:
-                callback.on_train_end(logs=logs)
-
-            for image, labels in tqdm(test_set, desc=f"test  {epoch} epoch"):
-                self.test_step(image, labels)
-            logs.update_test(self.acc_matrix.test_acc, self.test_loss)
-            for callback in self.callback:
-                callback.on_test_end(logs=logs)
-
-            for callback in self.callback:
-                callback.on_epoch_end(epoch, logs=logs)
-            if logs.stop_train:
-                break
-        for callback in self.callback:
-            callback.on_train_end(logs=logs)
